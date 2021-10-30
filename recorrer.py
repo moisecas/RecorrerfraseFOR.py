c80c8e3be0ac3616758ee989c390e592fb0ea5a6
moisecas <62753698+moisecas@users.noreply.github.com>
2021-10-29 21:03:32 -0500

    Add files via upload

---
+++ recorrer.py
@@ -0,0 +1,12 @@
+def run():
+    #nombre = input("escribe tu nombre: ")
+    #for letra in nombre: 
+        #print(letra) 
+        #para las letras vamos a imprimir cada letra en cada vuelta del ciclo
+    palabras = input("escribe lo que quiera:")
+    for caracter in palabras:
+        print(caracter.upper())
+
+
+if __name__ == "__main__": 
+    run()
